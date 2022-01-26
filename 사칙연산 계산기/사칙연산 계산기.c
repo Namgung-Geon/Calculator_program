b5ef50a0d9bb6d42ddddc902c575afe1cbacaa7c
Geon <a01021113503@gmail.com>
2022-01-26 22:46:23 +0900

    초기 커밋.

---
+++ 사칙연산 계산기/사칙연산 계산기.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	char c;
+
+	printf("사칙연산 계산기\n");
+	printf("덧셈 기호 : +, 뺄셈 기호 : -, 곱셈 기호 : *, 나눗셈 기호 : /\n");
+	scanf("%d%s%d", &a, &c, &b);
+	if (c =='+'){
+		printf("답 : %d\n", a + b);
+	}
+
+	if (c == '-') {
+		printf("답 : %d\n", a - b);
+	}
+
+	if (c == '*') {
+		printf("답 : %d\n", a * b);
+	}
+
+	if (c == '/') {
+		printf("답 : %d, 나머지 : %d", a / b, a % b);
+	}
+	
+	return 0;
+}
